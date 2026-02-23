a74cf0a447a4617d4c9d9b79445e382f8f13608c
Bruno Loureiro <BrunoLoureiroProfessional@proton.me>
2026-02-23 22:56:17 +0000

    animations

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withViewTransitions } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { routes } from './app.routes';
 import { authInterceptor } from '../core/interceptors/auth.interceptor';
@@ -7,7 +7,7 @@ import { authInterceptor } from '../core/interceptors/auth.interceptor';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes),
+    provideRouter(routes, withViewTransitions()),
     provideHttpClient(withInterceptors([authInterceptor]))
   ]
 };
